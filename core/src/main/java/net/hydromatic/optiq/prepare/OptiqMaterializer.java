e6920c8a2b7fde8c2ba430714c3107707adcb703
Jacques Nadeau <jacques@apache.org>
2014-07-06 13:39:53 -0700

    add alternative OptiqSchema
    Conflicts:
    	core/src/main/java/net/hydromatic/optiq/jdbc/OptiqSchema.java
    	core/src/main/java/net/hydromatic/optiq/prepare/OptiqMaterializer.java
    
    Resolve issue after rebase.
    
    Resolve rebasing conflicts. Disable the Lattice and corresponding test for now.

--- core/src/main/java/net/hydromatic/optiq/prepare/OptiqMaterializer.java
+++ core/src/main/java/net/hydromatic/optiq/prepare/OptiqMaterializer.java
@@ -120,6 +120,25 @@ class OptiqMaterializer extends OptiqPrepareImpl.OptiqPreparingStmt {
     return list;
   }
 
+  /** Returns the star tables defined in a schema.
+   * @param schema Schema */
+  private List<OptiqSchema.TableEntry> getStarTables(OptiqSchema schema) {
+    final List<OptiqSchema.TableEntry> list =
+        new ArrayList<OptiqSchema.TableEntry>();
+    // TODO: Assumes that star tables are all defined in a schema called
+    // "mat". Instead, we should look for star tables that use a given set of
+    // tables, regardless of schema.
+    final OptiqSchema matSchema = schema.root().getSubSchema("mat", true);
+    if (matSchema != null) {
+      for (OptiqSchema.TableEntry tis : matSchema.getTableEntries()) {
+        if (tis.getTable().getJdbcTableType() == Schema.TableType.STAR) {
+          list.add(tis);
+        }
+      }
+    }
+    return list;
+  }
+
   /** Implementation of {@link RelShuttle} that returns each relational
    * expression unchanged. It does not visit children. */
   static class RelNullShuttle implements RelShuttle {
